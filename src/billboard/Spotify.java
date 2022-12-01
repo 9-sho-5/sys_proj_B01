753454cf078eb2a995a0e98ab6ae45fd4e499424
9-sho-5 <hosho.kusu@icloud.com>
2022-12-01 22:02:23 +0900

    set env settinig

--- src/billboard/Spotify.java
+++ src/billboard/Spotify.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.nio.charset.StandardCharsets;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Spotify {
      
