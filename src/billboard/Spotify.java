eb151dba6ed6ceee7a0de5782622526379eff8b3
9-sho-5 <hosho.kusu@icloud.com>
2022-12-01 22:09:47 +0900

    fix: set private field to Spotify.java

--- src/billboard/Spotify.java
+++ src/billboard/Spotify.java
@@ -18,5 +18,33 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 public class Spotify {
      
+    // 環境変数の読み込み準備
+    Dotenv dotenv = Dotenv.configure().load();
+
+    // 環境変数からの読み込み
+    private final String clientId = dotenv.get("CLIENT_ID");
+	private final String clientSecret = dotenv.get("CLIENT_SECRET");
+	private final String redirectUri = dotenv.get("REDIRECT_URI");
+	private final String authorizeUrl = dotenv.get("AUTHORIZE_URL");
+	private final String apiEndpoint = dotenv.get("API_ENDPOINT");
+	private final String[] scope = {
+		"playlist-read-private",
+		"playlist-modify-private",
+		"playlist-read-collaborative",
+		"playlist-modify-public",
+		"user-read-email",
+		"user-read-private",
+		"user-modify-playback-state",
+		"user-read-playback-state",
+		"user-read-currently-playing",
+		"user-read-recently-played",
+		"user-read-playback-position",
+		"user-top-read",
+	};
+    // ユーザーのAPI認証後に取得するcodeの格納変数
+    private String code = null;
+    // アクセストークンの格納変数
+	private String accessToken = null;
+    
 }
 
