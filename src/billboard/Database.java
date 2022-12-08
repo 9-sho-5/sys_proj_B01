32dfd6a3f7c6c6eeb2469fbf22c68d222081318a
9-sho-5 <hosho.kusu@icloud.com>
2022-12-09 04:25:57 +0900

    fix: MySQL -> SQLite

--- src/billboard/Database.java
+++ src/billboard/Database.java
@@ -9,23 +9,30 @@ import java.sql.Statement;
 public class Database {
 
     // MySQLの接続設定
-    static final String DB_URL = "jdbc:mysql://localhost/";
-    static final String USER = "root";
-    static final String PASS = "";
+    private final static String DB_URL = "jdbc:sqlite:sys_proj_B01.sqlite3";
 
     /**
      * データベースの初期設定
      * データベースの作成、テーブルの作成を行う
      */
     public static void setUp() {
+
+        // ライブラリのパス設定
+        try {
+            Class.forName("org.sqlite.JDBC");
+            System.out.println("set lib path");
+        } catch (Exception e) {
+            e.getMessage();
+        }
+
         // データベースとの接続
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        try (Connection conn = DriverManager.getConnection(DB_URL);
                 Statement stmt = conn.createStatement();) {
 
             System.out.println("connected Database successfully...");
 
             // データベースの作成
-            String sql = "CREATE DATABASE sys_proj_B01;";
+            String sql = "sqlite3 sys_proj_B01.sqlite3;";
             try {
                 stmt.executeUpdate(sql);
                 System.out.println("created Database successfully...");
@@ -33,23 +40,13 @@ public class Database {
                 System.out.println(e.getMessage());
             }
 
-            // 作成したデータベースの使用
-            sql = "USE sys_proj_B01; ";
-            try {
-                stmt.executeUpdate(sql);
-                System.out.println("use Database successfully...");
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-
             // テーブルの作成
             sql = "CREATE TABLE Ranking (\n"
-                    + "id INT(11) AUTO_INCREMENT NOT NULL, "
-                    + "track_name VARCHAR(255) NOT NULL ,"
-                    + "artist_name VARCHAR(255) NOT NULL ,"
-                    + "album_name VARCHAR(255) NOT NULL ,"
-                    + "album_image_url VARCHAR(255) NOT NULL ,"
-                    + "PRIMARY KEY (id))";
+                    + "id integer primary key autoincrement,"
+                    + "track_name text,"
+                    + "artist_name text,"
+                    + "album_name text,"
+                    + "album_image_url text);";
             try {
                 stmt.executeUpdate(sql);
                 System.out.println("Table created successfully...");
@@ -59,103 +56,54 @@ public class Database {
             stmt.close();
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("could not connected Database...");
         }
     }
 
     /**
-     * データベースの削除
+     * デーブルの削除
      */
     public static void drop() {
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        try (Connection conn = DriverManager.getConnection(DB_URL);
                 Statement stmt = conn.createStatement();) {
 
             System.out.println("connected Database successfully...");
 
             // データベースの削除
-            String sql = "drop DATABASE sys_proj_B01;";
+            String sql = "drop table Ranking;";
             try {
                 stmt.executeUpdate(sql);
-                System.out.println("created Database successfully...");
+                System.out.println("drop table Ranking successfully...");
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
             stmt.close();
         } catch (SQLException e) {
             e.printStackTrace();
+            System.out.println("could not connected Database...");
         }
-    }
-
-    /**
-     * データベースの存在確認
-     * @return
-     */
-    public static boolean check() {
-        boolean flg = false;
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                Statement stmt = conn.createStatement();) {
-
-            // データベースの作成
-            String sql = "CREATE DATABASE sys_proj_B01;";
-            try {
-                stmt.executeUpdate(sql);
-                System.out.println("created Database successfully...");
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-
-            // 作成したデータベースの使用
-            sql = "USE sys_proj_B01; ";
-            try {
-                stmt.executeUpdate(sql);
-                System.out.println("use Database successfully...");
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-
-            // データベースの存在確認
-            sql = "select database()";
-            try {
-                ResultSet rs = stmt.executeQuery(sql);
-                while(rs.next()) {
-                    String name = rs.getString("database()");
-                    System.out.println(name + " is selected");
-                }
-                flg = true;
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-            stmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return flg;
     }
 
     /**
      * データの挿入
+     * 
      * @param track_name
      * @param artist_name
      * @param album_name
      * @param album_image_url
      */
-    public static void insertData(String track_id, String track_name, String artist_name, String album_name, String album_image_url) {
+    public static void insertData(String track_id, String track_name, String artist_name, String album_name,
+            String album_image_url) {
 
         System.out.println("=== insert Data ===");
 
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        try (Connection conn = DriverManager.getConnection(DB_URL);
                 Statement stmt = conn.createStatement();) {
 
-            // 作成したデータベースの使用
-            String sql = "USE sys_proj_B01; ";
-            try {
-                stmt.executeUpdate(sql);
-                System.out.println("use Database successfully...");
-            } catch (SQLException e) {
-                System.out.println(e.getMessage());
-            }
-
             // データの挿入
-            sql = String.format("insert into Ranking values (0, '%s', '%s', '%s', '%s');", track_name, artist_name, album_name, album_image_url);
+            String sql = String.format(
+                    "insert into Ranking(track_name, artist_name, album_name, album_image_url) values ('%s', '%s', '%s', '%s');",
+                    track_name, artist_name, album_name, album_image_url);
             try {
                 stmt.executeUpdate(sql);
                 System.out.println("inserted data completely!!");
