8cad6c6d61e295d4db914954b5847767ee084743
caltistals <y.db.qp.y@gmail.com>
2023-01-03 12:25:05 +0900

    fix: 検索画面へ戻るリンクのスタイルを修正

--- WebContent/billboard/assets/js/result.js
+++ WebContent/billboard/assets/js/result.js
@@ -45,7 +45,24 @@ const Result = () => {
                   <p className="loader">Loading...</p>
                 </>
               )}
-              <a href="./search.html">検索画面へ戻る</a>
+              <a className="back-to-search" href="./search.html">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="icon icon-tabler icon-tabler-arrow-back"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="#2c3e50"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M9 11l-4 4l4 4m-4 -4h11a4 4 0 0 0 0 -8h-1" />
+                </svg>
+                <span>検索画面へ戻る</span>
+              </a>
             </div>
           </>
         )}
